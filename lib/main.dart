0afa409fde9f8775c64dc73c90532fd33e0f88a1
Ananta Roy <ananta6d595@gmail.com>
2022-08-14 08:13:15 +0600

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:developer' as devTool
+    show
+        log; // "show" for limiting to specific funstion and "as" differentiate fro same function from different packages.
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -45,23 +49,13 @@ class HomePage extends StatelessWidget {
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                print('Email is verified');
+                return const NotesView();
               } else {
                 return const VerifyEmailView();
               }
             } else {
               return const LoginView();
             }
-            return const Text('Done');
-            // print(user);
-            // if (user?.emailVerified ?? false) {
-            //   print("You are a verified user");
-            //   return Text('Done');
-            // } else {
-            //   print("You need to verify your email first");
-            //   return VerifyEmailView();
-            // }
-            return const LoginView();
           default:
             return const CircularProgressIndicator();
         }
@@ -69,3 +63,79 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('Log out'),
+                )
+              ];
+            },
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldlogout = await showLogOutDialog(context); // shouldlogout takes pop functions output 
+                  if (shouldlogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/login', (_) => false);
+                  }
+                  break;
+              }
+            },
+          ),
+        ],
+      ),
+      body: const Text('Hello World'),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign out'),
+        content: const Text('Are you sure to sign out?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false); // the false in pop return as value 
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true); // the true in pop function returns as value
+            },
+            child: const Text('Log Out'),
+          )
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
+}
+
+
+// For switch case every case should return somthing or should have brerak statement. 
+// if there is any "if" condition in any "case" and not return or break keyword in any "case"
+// then every "if" condition must have return statement or break keyword. 
